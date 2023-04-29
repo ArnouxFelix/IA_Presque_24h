b200970eff3dc65fa70513a45ad24341655d306e
Vincent-Tison <v.tison2205@gmail.com>
2023-04-29 20:50:50 +0200

    ajout fonction choix nain selon moins bonne case

--- IA_Presque_24h/metier/nain/Nain.cs
+++ IA_Presque_24h/metier/nain/Nain.cs
@@ -1,4 +1,5 @@
-﻿using IA_Presque_24h.metier.carte;
+﻿using IA_Presque_24h;
+using IA_Presque_24h.metier.carte;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,5 +15,17 @@ namespace IA_Presque_24h.metier.nain
 
         public Case Case { get => _case; set => _case = value; }
         public int NvPioche { get => nvPioche; set => nvPioche = value; }
+        public Nain ChoixNain(List<Nain> nain)
+        {
+            Nain nainADeplacer = nain[0];
+            for (int i = 0; i < nain.Count; i++)
+            {
+                if (nainADeplacer.Case.ValeurTotale > nain[i].Case.ValeurTotale)
+                {
+                    nainADeplacer = nain[i];
+                }
+            }
+            return nainADeplacer;
+        }
     }
 }
