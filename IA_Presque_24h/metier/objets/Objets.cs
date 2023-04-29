1b017622f52efd4f267a06939fdaa24d4b5f6668
Djohana <djohana_tonnelier@etu.u-bourgogne.fr>
2023-04-29 16:21:52 +0200

    supp minerais +  add using

--- IA_Presque_24h/metier/objets/Objets.cs
+++ IA_Presque_24h/metier/objets/Objets.cs
@@ -1,6 +1,9 @@
 using IA_Presque_24h.metier.carte;
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 namespace IA_Preque_24h.metier.objets
 {
     public abstract class Objet
